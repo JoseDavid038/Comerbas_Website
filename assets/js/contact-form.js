6e8d2cb340d81d6a6c32c83f6ec8a1ecbd766302
Jose David Gonzalez <programacionjose2@gmail.com>
2025-03-28 15:07:12 -0500

    Finished ips.html mobile version

--- assets/js/contact-form.js
+++ assets/js/contact-form.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', function() {
   loadComponent("contact-form", "/components/contact-form.html" , () => {
-    
+    changeTitle();
 
   });
   
@@ -15,4 +15,19 @@ function loadComponent(tag, file, callback = null){
         if (callback) callback();  // Ensure the callback runs after loading
       })
       .catch(error => console.error(`Error loading ${file}:`,error));
+}
+
+
+
+function changeTitle(){
+   const titleForm = document.querySelector('.js-title-form');
+   const paragraphForm = document.querySelector('.js-paragraph-form');
+
+   if (window.location.pathname.includes("ips.html")|| window.location.pathname.includes("nosotros.html") ){
+    titleForm.textContent = "Mejore la gestión de salud ocupacional en su empresa";
+    paragraphForm.textContent = "Contamos con profesionales certificados y tecnología avanzada para realizar evaluaciones precisas, rápidas y confiables.";
+   }else{
+    titleForm.textContent = "Contáctanos";
+    paragraphForm.textContent = "Estamos felices en brindarte ayuda en lo que necesites.";
+   }
 }
--- assets/js/section.js
+++ assets/js/section.js
@@ -29,11 +29,23 @@ async function loadComponent (selector, componentPath, data = {}) {
 
 
 
+
 loadComponent("#section-1", "/components/section.html", {
   image: "/assets/images/services/service-optometria-test.png",
   title: "Optometria laboral",
-  paragraph: "Ofrecemos evaluaciones precisas y certificadas permitiéndole a su empresa tomar decisiones informadas y garantizar un entorno laboral seguro",
+  paragraph: "Ofrecemos evaluaciones precisas y certificadas permitiéndole a su empresa tomar decisiones informadas y garantizar un entorno laboral seguro.",
   list: ["Evaluación completa de la agudeza visual lejana y cercana", "Pruebas de profundidad visual y discriminación de colores", "Recomendaciones específicas según el puesto de trabajo", "Prevención de riesgos visuales ocupacionales"],
   buttonText: "Contáctanos para saber más",
   buttonLink: "/index.html"
 });
+
+
+loadComponent("#section-2", "/components/section.html", {
+  image: "/assets/images/services/service-psicology.png",
+  title: "Psicología Laboral",
+  paragraph: "Evaluamos los factores psicosociales que impactan en el desempeño y bienestar de sus colaboradores, identificando fortalezas y áreas de mejora para optimizar el ambiente laboral.",
+  list: ["Evaluación de Riesgos Bio-Psicosociales", "Pruebas Psicotécnicas Laborales", "Capacitación en Salud Mental Laboral", "Control de Sustancias psicoactivas y Alcoholimetría."],
+  buttonText: "Contáctanos para saber más",
+  buttonLink: "/index.html"
+});
+
